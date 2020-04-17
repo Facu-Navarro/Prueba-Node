3badab8700e7acdbd93bd91e0a50c535ca46d74b
Facu-navarro <facunava710@gmail.com>
2020-04-17 19:05:42 -0300

    cambios en index controller y en index routes
    cambios en index controller y en index routes

--- Tarea3/controllers/indexController.js
+++ Tarea3/controllers/indexController.js
@@ -1,8 +1,8 @@
 const controlador = {
 
     Main : (req, res) => {
-        let home = "<h1> Ni Superman, IronMan o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!. </h1>";
-        res.send(home);
+        let home = { main : "<h1> Ni Superman, IronMan o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!. </h1>"};
+        res.send('index', home);
     },
 
 
