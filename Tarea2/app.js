a31ddf276d423c6b69b705e941d7a973ab32d893
Facu-navarro <facunava710@gmail.com>
2020-04-17 17:30:26 -0300

    Actualizacion de Tarea2, y pausa

--- Tarea2/app.js
+++ Tarea2/app.js
@@ -1,1 +1,49 @@
 //Punto de entrada!
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+// rutas
+const rutasHeroes = require("./routes/heroes");
+const rutasMain = require("./routes/main");
+
+// usar rutas
+app.use("/", rutasHeroes);
+app.use("/", rutasMain);
+
+
+// --------------------------------------------------------------------------------------------------------
+
+// Levantando el Servidor en el puerto 3030
+app.listen(3030, () => console.log('Server running in 3030 port'));
+
+/*
+// Ruta /heroes/n ➝ se envía el nombre y profesión del héroe solicitado
+app.get('¿?', (¿?) => {
+	// Acá lo primero será encontrar al héroe que corresponda
+	let heroe = __¿?__;
+	
+	// Si se encuentra al héroe se envía el nombre y su profesión
+	// Si NO se encuentra se envía el mensaje de no encontrado
+});
+
+// Ruta /heroes/n/bio ➝ se envía la bio del héroe solicitado
+app.get('¿?', (¿?) => {
+	// Acá lo primero será encontrar al héroe que corresponda
+	let heroe = __¿?__;
+
+	// Si NO se encuentra al héroe se envía un mensaje
+	// Si se encuentra al héroe:
+		// Se pregunta si vino el parámetro Y el valor esperado y se envía la información
+		// Si nó vino el parámetro se envía el mensaje de error
+	}
+});
+
+// Ruta Créditos
+// ¿?
+
+// Ruta... ¿Pára qué sirve esto?
+app.get('*', (req, res) => {
+	res.status(404).send('404 not found. <br> ¡Houston, poseemos problemas!');
+});  
+*/
---
+++ Tarea2/routes/heroes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const heroesController = require("../controllers/heroesController");
+
+router.get("/heroes", heroesController.main);
+
+router.get("/heroes/:id", heroesController.heroe);
+
+router.get("/heroes/:id/bio/:ok?", heroesController.heroeCompleto);
+
+module.exports = router;
